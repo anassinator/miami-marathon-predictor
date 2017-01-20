e8b886e923d842f635d074f26c0421fc3bbda635
Anass Al <mr@anassinator.com>
2017-01-19 20:08:55 -0500

    Add filter_by_gender()

--- data.py
+++ data.py
@@ -23,6 +23,20 @@ def get_unignored_data():
     return df[df.ignore == False]
 
 
+def filter_by_gender(df, male):
+    """Filters the data by gender.
+
+    Args:
+        df: DataFrame.
+        male: True if male, False otherwise.
+
+    Returns:
+        DataFrame.
+    """
+    gender = 1 if male else 0
+    return df[df.male == gender]
+
+
 if __name__ == "__main__":
     total = get_data().count().id
     unignored = get_unignored_data().count().id
